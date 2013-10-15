b3b6ddfe34f723212bc14833cf8d6ea9be55acd7
John Pearson <jmxpearson@gmail.com>
2013-10-15 15:03:08 -0400

    modified Q to fit basic RL model

--- Q_model.m
+++ Q_model.m
@@ -1,22 +1,22 @@
-function out=Q_model(beta,choice,outcome)
-%calculate the action values, given model parameters
-%beta, choices, and outcomes
-%beta is a learning rate
+function out=Q_model(alpha,choice,outcome)
+% basic reinforcement learning model
+% calculate the action values, given model parameters
+% alpha is the learning rate
 
 N=length(outcome); %number of trials
 k=length(unique(choice)); %number of options
 Q=nan(N,k); %values of each choice each trial
 Qthis=nan(N,1); %value of chosen option
 
-Q(1,:)=0; %initialize guesses
-Qthis(1)=0;
-for ind=2:N
+Q(1,:) = 0; %initialize guesses
+Qthis(1) = 0;
+for ind = 2:N
     %old business: bring forward action values from last trial
-    Q(ind,:)=Q(ind-1,:); %all values set to those for last trial
-    Qthis(ind)=Q(ind,choice(ind)); %value we based our decision this trial on
+    Q(ind,:) = Q(ind-1,:); %all values set to those for last trial
+    Qthis(ind) = Q(ind,choice(ind)); %value we based our decision this trial on
     
     %now do learning update for chosen value
-    Q(ind,choice(ind)) = Q(ind,choice(ind)) + beta*(outcome(ind)-Q(ind,choice(ind))); 
+    Q(ind,choice(ind)) = Q(ind,choice(ind)) + alpha*(outcome(ind)-Q(ind,choice(ind))); 
     
 end
 
