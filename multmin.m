4dfc7b3728659e9d1e07c4b3ef6dbfa39a7b1949
John Pearson <jmxpearson@gmail.com>
2013-10-17 16:02:28 -0400

    edits to multmin to handle ill-conditioned pars
    Some random parameter sets were causing issues internally with
    fmincon. This fixes that by changing from a fixed iteration to a
    while loop with try/catch inside.

--- multmin.m
+++ multmin.m
@@ -2,12 +2,18 @@ function [beta,fval]=multmin(fhandle,lb,ub,numiter,options)
 % runs multiple minimizations, so as not to get trapped in local min
 % chooses random starting points uniformly between allowed bounds
 
-for ind=1:numiter
-    bthis=lb+rand(1,numel(ub)).*(ub-lb); %seed random starting point
-    if exist('options','var')
-        [beta(ind,:),fval(ind)]=fmincon(fhandle,bthis,[],[],[],[],lb,ub,[],options);
-    else
-        [beta(ind,:),fval(ind)]=fmincon(fhandle,bthis,[],[],[],[],lb,ub,[]);
+ind = 1;
+while ind <= numiter
+    try
+        bthis=lb+rand(1,numel(ub)).*(ub-lb); %seed random starting point
+        if exist('options','var')
+            [beta(ind,:),fval(ind)]=fmincon(fhandle,bthis,[],[],[],[],lb,ub,[],options);
+        else
+            [beta(ind,:),fval(ind)]=fmincon(fhandle,bthis,[],[],[],[],lb,ub,[]);
+        end
+        ind = ind + 1;
+    catch
+        continue
     end
 end
 
