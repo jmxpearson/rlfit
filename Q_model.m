14ed1d3e566f8e68041156d20ce23c53692e3bbd
John Pearson <jmxpearson@gmail.com>
2014-02-13 14:34:16 -0500

    removed redundant lines in Q_model

--- Q_model.m
+++ Q_model.m
@@ -9,11 +9,10 @@ Q=nan(N,k); %values of each choice each trial
 Qthis=nan(N,1); %value of chosen option
 
 Q(1,:) = 0; %initialize guesses
-Qthis(1) = 0;
+
 for ind = 2:N
     %old business: bring forward action values from last trial
     Q(ind,:) = Q(ind-1,:); %all values set to those for last trial
-    Qthis(ind) = Q(ind,choice(ind)); %value we based our decision this trial on
     
     %now do learning update for chosen value
     Q(ind,choice(ind)) = Q(ind,choice(ind)) + alpha*(outcome(ind)-Q(ind,choice(ind))); 
